876c3e93760636f2be795ba7455050ccb95339ea
Amol Chandra <50043942+1amol2@users.noreply.github.com>
2023-07-03 15:17:42 +0530

    Update NoteActivity.java

--- app/src/main/java/com/amol/app/noteit/activities/NoteActivity.java
+++ app/src/main/java/com/amol/app/noteit/activities/NoteActivity.java
@@ -2,10 +2,7 @@ package com.amol.app.noteit.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.os.Handler;
-
-
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.amol.app.noteit.databinding.ActivityNoteBinding;
